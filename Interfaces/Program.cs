823cbff1774938b753165d3b57b4aa2245e1ff6f
taner <taner@DESKTOP-8FBSMG9>
2021-09-05 00:49:52 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager= new CustomerManager();
+            customerManager.add();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+    interface IPersonManager
+    {
+        public void add();
+        public void update();
+      
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void add()
+        {
+            Console.WriteLine("müşteri  eklendi");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Müşsteri güncellendi");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void add()
+        {
+            Console.WriteLine("çalışan eklendi");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Çalışan durumu güncellendi");
+        }
+    }
+    class ProjectManager
+    {
+        public void add()
+        {
+            Console.WriteLine();
+        }
+    }
+}
